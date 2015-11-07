aa265ae862a9d7c49eee04d6f2d8119ff52ebbc2
matt <matt@matt-desktop>
2015-11-06 22:57:31 -0700

    added class matt

---
+++ DynamicSequence/src/matt.cpp
@@ -0,0 +1,18 @@
+/*
+ * matt.cpp
+ *
+ *  Created on: Nov 6, 2015
+ *      Author: matt
+ */
+
+#include "matt.h"
+
+matt::matt() {
+	// TODO Auto-generated constructor stub
+
+}
+
+matt::~matt() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DynamicSequence/src/matt.h
@@ -0,0 +1,17 @@
+/*
+ * matt.h
+ *
+ *  Created on: Nov 6, 2015
+ *      Author: matt
+ */
+
+#ifndef MATT_H_
+#define MATT_H_
+
+class matt {
+public:
+	matt();
+	virtual ~matt();
+};
+
+#endif /* MATT_H_ */
